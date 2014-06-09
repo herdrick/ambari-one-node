f091a28cd4bd9e64edc2af998e186a73577c74ba
Ethan Herdrick <info@reatlas.com>
2014-06-09 16:13:46 -0700

    generalize 'waiting until an url is responsive' into a function.

--- single_node_ambari.sh
+++ single_node_ambari.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+function wait_until_some_http_status () {
+    url=$1
+    target_status=$2
+    s=0
+    while [ $s != $target_status ]; do
+        s=$(curl -o /dev/null -s -w %{http_code} $url)
+        if [ $s == '000' ]
+        then
+            echo "<no response from server>"
+        else
+            echo "HTTP status: $s"
+        fi
+        sleep 1
+    done
+}
+
 sudo yum install -y ntp ntpdate ntp-doc
 # looks like we already had ntpd and ntpupdate installed
 sudo chkconfig ntpd on
@@ -11,28 +27,20 @@ sudo ambari-agent restart
 sudo ambari-server setup -v -s
 sudo ambari-server restart
 
-s=0
-echo "Trying http://localhost:8080/api/v1/blueprints to confirm server is up... (HTTP status 000 = not up)"
-while [ $s != '200' ]; do
-    sleep 1
-    s=$(curl -o /dev/null -s -w %{http_code} http://admin:admin@localhost:8080/api/v1/blueprints)
-    echo "HTTP status: $s"
-done
+echo "trying http://localhost:8080/api/v1/blueprints to confirm Ambari server is up..."
+wait_until_some_http_status "http://admin:admin@localhost:8080/api/v1/blueprints" "200"
 sleep 2
 
 curl -v -X POST -d @blueprint-big-1.json http://admin:admin@localhost:8080/api/v1/blueprints/bp-all-services --header "Content-Type:application/json" --header 'X-Requested-By:mycompany'
+
+echo "trying http://localhost:8080/api/v1/clusters to confirm Ambari server is still up..."
+wait_until_some_http_status "http://admin:admin@localhost:8080/api/v1/clusters" "200"
+
 my_fqdn=$(hostname -f)
 sed s/FQDN_GOES_HERE/$my_fqdn/ cluster-creation-raw.json > cluster-creation.json
 
-s=0
-echo "trying http://localhost:8080/api/v1/blueprints to confirm server is ready..."
-while [ $s != '200' ]; do
-    sleep 2
-    s=$(curl -o /dev/null -s -w %{http_code} http://admin:admin@localhost:8080/api/v1/blueprints)
-    echo "HTTP status: $s"
-done
-
 curl http://admin:admin@localhost:8080/api/v1/hosts
-echo 'pausing for 15 seconds to let server settle down'
+
+echo 'pausing for 15 seconds to let Ambari server settle down'
 sleep 15
 curl -v -X POST -d @cluster-creation.json http://admin:admin@localhost:8080/api/v1/clusters/cl1 --header "Content-Type:application/json" --header 'X-Requested-By:mycompany'
