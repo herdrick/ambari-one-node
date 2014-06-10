53e61462cc3dc432747afe366c6de06c5a0d548b
Ethan Herdrick <info@reatlas.com>
2014-06-09 18:46:49 -0700

    add more logging

--- single_node_ambari.sh
+++ single_node_ambari.sh
@@ -16,10 +16,11 @@ function wait_until_some_http_status () {
     done
 }
 
-sudo yum install -y ntp ntpdate ntp-doc
-# looks like we already had ntpd and ntpupdate installed
+echo "First install and start needed services"
+sudo yum install -y ntp ntpdate ntp-doc  # in many installs, ex. RHEL EC2 AMI, we already have ntpd and ntpupdate installed
 sudo chkconfig ntpd on
 sudo service ntpd restart
+echo "To get ambari with yum we need the Hortonworks repo"
 sudo wget -P /etc/yum.repos.d/ http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.6.0/ambari.repo
 sudo yum -y install ambari-server ambari-agent
 
@@ -31,17 +32,20 @@ echo "Trying http://localhost:8080/api/v1/blueprints to confirm Ambari server is
 wait_until_some_http_status "http://admin:admin@localhost:8080/api/v1/blueprints" "200"
 sleep 2 # wait a few moments longer just to let the server settle down
 
+echo "Add the blueprint-big-1.json blueprint file to our Ambari server's available blueprints"
 curl -v -X POST -d @blueprint-big-1.json http://admin:admin@localhost:8080/api/v1/blueprints/bp-all-services --header "Content-Type:application/json" --header "X-Requested-By:mycompany"
 
 echo "Trying http://localhost:8080/api/v1/clusters to confirm Ambari server is still up..."
 wait_until_some_http_status "http://admin:admin@localhost:8080/api/v1/clusters" "200"
 
+echo "Replace the dummy hostname in the cluster creation JSON file with this host's fully qualified domain name"
 my_fqdn=$(hostname -f)
 sed s/FQDN_GOES_HERE/$my_fqdn/ cluster-creation-raw.json > cluster-creation.json
 
 echo ""
 echo "Pausing for 15 seconds to let Ambari server settle down"
 sleep 15
+echo "Now cause a cluster to be created with our loaded blueprint"
 curl -v -X POST -d @cluster-creation.json http://admin:admin@localhost:8080/api/v1/clusters/cl1 --header "Content-Type:application/json" --header "X-Requested-By:mycompany"
 echo ""
 echo "Single node Ambari setup finished. Point browser to localhost:8080 and log in as admin:admin to use Ambari."
